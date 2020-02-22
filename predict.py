d46582cb7fa4ff60f021121c4176ec926cf9d88c
Martijn de Heus <mjdeheus@hotmail.com>
2020-02-22 23:29:40 +0100

    Add initial try

---
+++ predict.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix 
+from sklearn.svm import SVC
+
+subjective_sentences_file = "data/Steve+Rhodes/subj.Steve+Rhodes"
+four_class_labels_file = "data/Steve+Rhodes/label.4class.Steve+Rhodes"
+
+subjective_sentences = []
+word_to_index = {}
+labels = []
+
+with open(subjective_sentences_file) as f:
+  for line in f:
+    subjective_sentences.append(line)
+    words = list(set(line.split(' ')))
+    for word in words:
+      word = word.strip()
+      if word not in word_to_index:
+        word_to_index[word] = len(word_to_index)
+
+with open(four_class_labels_file) as f:
+  for line in f:
+    labels.append(int(line))
+
+feature_vectors = []
+
+for sentence in subjective_sentences:
+  feature_vector = np.zeros(len(word_to_index))
+  words = list(set(line.split(' ')))
+  for word in words:
+    word = word.strip()
+    feature_vector[word_to_index[word]] = 1
+    feature_vectors.append(feature_vector)
+
+assert len(feature_vectors) == len(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(feature_vectors, labels, test_size=0.5, random_state = 0)
+
+assert len(X_train) == len(y_train)
+assert len(X_test) == len(y_test)
+
+svm_model = SVC(kernel="linear", C=1).fit(X_train, y_train)
+svm_predictions = svm_model.predict(X_test) 
+  
+accuracy = svm_model.score(X_test, y_test) 
+print(accuracy)
+
+cm = confusion_matrix(y_test, svm_predictions)
+print(cm)
