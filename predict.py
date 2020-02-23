0f788f83e58664e3f8bb89d225a530f6511cbf4b
Daan Oudejans <D.A.J.Oudejans@student.tudelft.nl>
2020-02-23 12:32:26 +0100

    Add linear support vector regression

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVR
 
 subjective_sentences_file = "data/Steve+Rhodes/subj.Steve+Rhodes"
 four_class_labels_file = "data/Steve+Rhodes/label.4class.Steve+Rhodes"
@@ -43,11 +44,16 @@ X_train, X_test, y_train, y_test = train_test_split(feature_vectors, labels, tes
 assert len(X_train) == len(y_train)
 assert len(X_test) == len(y_test)
 
-svm_model = OneVsRestClassifier(SVC(kernel="linear", C=1)).fit(X_train, y_train)
+# One-vs-All classifier
+# svm_model = OneVsRestClassifier(SVC(kernel="linear", C=1)).fit(X_train, y_train)
+
+# Linear, epsilon-intensive support vector regression
+svm_model = LinearSVR(epsilon=0.1).fit(X_train, y_train) # Not sure what epsilon should be
+
 svm_predictions = svm_model.predict(X_test)
 
 accuracy = svm_model.score(X_test, y_test)
 print(accuracy)
 
-cm = confusion_matrix(y_test, svm_predictions)
-print(cm)
+# cm = confusion_matrix(y_test, svm_predictions)
+# print(cm)
