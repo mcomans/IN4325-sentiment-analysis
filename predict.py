e86734d295bdc5ee7286b62d4fa4d0912df66ea3
Daan Oudejans <D.A.J.Oudejans@student.tudelft.nl>
2020-02-24 16:49:07 +0100

    Write all results to csv

--- predict.py
+++ predict.py
@@ -38,7 +38,7 @@ def read_labels(labels_filepath):
 
 
 def classify_ova(X_train, X_test, y_train, y_test):
-    print("Running One-vs-All classifier...")
+    print("> Running One-vs-All classifier...")
     svm_model = OneVsRestClassifier(SVC(kernel="linear", C=1)).fit(X_train, y_train)
 
     svm_predictions = svm_model.predict(X_test)
@@ -49,17 +49,20 @@ def classify_ova(X_train, X_test, y_train, y_test):
     cm = confusion_matrix(y_test, svm_predictions)
     print(cm)
 
+    return accuracy
+
 
 def regression(X_train, X_test, y_train, y_test):
-    print("Running linear support vector regression...")
+    print("> Running linear support vector regression...")
     svm_model = LinearSVR(epsilon=0.1).fit(X_train, y_train)  # Not sure what epsilon should be
 
     accuracy = svm_model.score(X_test, y_test)
     print(accuracy)
     # Maybe we should create a confusion matrix as well for regression with rounded values
+    return accuracy
 
 
-def run(feature_vectors, labels):
+def run(author, nr_classes, feature_vectors, labels):
     assert len(feature_vectors) == len(labels)
 
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, labels, test_size=0.5,
@@ -68,12 +71,18 @@ def run(feature_vectors, labels):
     assert len(X_train) == len(y_train)
     assert len(X_test) == len(y_test)
 
-    classify_ova(X_train, X_test, y_train, y_test)
-    regression(X_train, X_test, y_train, y_test)
+    reg_accuracy = regression(X_train, X_test, y_train, y_test)
+    ova_accuracy = classify_ova(X_train, X_test, y_train, y_test)
 
+    with open('results.csv', 'a') as f:
+        f.write(f"{author},{nr_classes},{reg_accuracy},{ova_accuracy}\n")
+
+
+with open('results.csv', 'w') as f:
+    f.write('author,nr_classes,reg_accuracy,ova_accuracy\n')
 
 for author_name in subjective_sentences_files:
-    print(f"Using {author_name} dataset")
+    print(f">>> Using {author_name} dataset")
     subjective_sentences_file = subjective_sentences_files[author_name]
     three_class_labels_file = three_class_labels_files[author_name]
     four_class_labels_file = four_class_labels_files[author_name]
@@ -101,9 +110,8 @@ for author_name in subjective_sentences_files:
 
         feature_vectors.append(feature_vector)
 
-    print("With three class labels")
-    run(feature_vectors, read_labels(three_class_labels_file))
+    print(">> With three class labels")
+    run(author_name, 3, feature_vectors, read_labels(three_class_labels_file))
 
-    print("With four class labels")
-    run(feature_vectors, read_labels(four_class_labels_file))
-
+    print(">> With four class labels")
+    run(author_name, 4, feature_vectors, read_labels(four_class_labels_file))
